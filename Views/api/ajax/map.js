9eca823a865f411e0d0b10e1fb2998e26dc6f924
RootGeorge <g.sopov@edu.salford.ac.uk>
2024-04-10 13:23:01 +0100

    bugs fixed

--- Views/api/ajax/map.js
+++ Views/api/ajax/map.js
@@ -60,18 +60,3 @@ if (navigator.geolocation) {
     // If geolocation is not supported by the browser
     console.error('Geolocation is not supported by this browser.');
 }
-
-// Function to add event listeners to "Show on Map" buttons
-function addShowOnMapEventListeners() {
-    var showOnMapButtons = document.querySelectorAll('.show-on-map');
-    showOnMapButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            var lat = parseFloat(this.dataset.lat);
-            var lng = parseFloat(this.dataset.lng);
-            map.setView([lat, lng], 16);
-        });
-    });
-}
-
-// Call the function after the HTML content has finished loading
-document.addEventListener('DOMContentLoaded', addShowOnMapEventListeners);
